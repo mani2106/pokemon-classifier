57b92cfe267ab98dd9a6f79d523c2fa3d9901bd9
Manimaran Paneerselvam <maxxmaran001@gmail.com>
2019-08-26 21:49:41 +0530

    - Added development startup batch file.  - Added comments explaining code in brief.

--- server.py
+++ server.py
@@ -1,14 +1,101 @@
-from flask import Flask
-from flask import render_template
+import asyncio, aiohttp
+import aiohttp
+import asyncio
+import uvicorn
+from fastai import *
+from fastai.vision import *
+from io import BytesIO
+from starlette.applications import Starlette
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import HTMLResponse, JSONResponse
+from starlette.staticfiles import StaticFiles
 
-app = Flask(__name__)
+app = Starlette()
+app.add_middleware(CORSMiddleware, allow_origins=[
+                   '*'], allow_headers=['X-Requested-With', 'Content-Type'])
+app.mount('/static', StaticFiles(directory='static'))
+# app.wsgi_app = ProxyFix(app.wsgi_app)
+
+# export_file_url = r'https://is.gd/acfAOG'
+export_file_url = r"https://is.gd/flgMdy"
+export_file_name = 'pokemon_resnet18_73acc.pkl'
+
+path = Path(__file__).parent
+
+# Read class names from file
+file_path = Path("pokemons.txt")
+
+with open(file_path) as file:
+    classes = file.readlines()
 
 
-@app.route('/hello')
-def hello_world():
-    return 'Hello, World!'
+async def download_file(url, dest):
+    if dest.exists():
+        return
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            model_pkl = await response.read()
+            with open(dest, 'wb') as f:
+                f.write(model_pkl)
+
+
+async def setup_learner():
+    """Downloads the learner from link and saves it for the session"""
+    await download_file(export_file_url, path / export_file_name)
+    try:
+        model = load_learner(path, export_file_name)
+        return model
+    except RuntimeError as e:
+        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+            print(e)
+            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\
+                        \n\nPlease update the fastai library in your training environment and export your model again.\
+                        \n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+            raise RuntimeError(message)
+        else:
+            raise
+
+
+# loop = asyncio.get_event_loop()
+# # Setup learner task
+# tasks = [asyncio.ensure_future(setup_learner())]
+# # get model object
+# learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
+# loop.close()
+
+loop = asyncio.get_event_loop()
+# Setup learner task
+tasks = [asyncio.ensure_future(setup_learner())]
+# get model object
+learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
+loop.close()
+
+# @app.route('/hello')
+# def hello_world():
+#     return 'Hello, World!'
 
 
 @app.route('/')
-def home():
-    return render_template("index.html")
+def home(request):
+    # return render_template("index.html")
+    html_file = path / 'templates' / 'index.html'
+    return HTMLResponse(html_file.open().read())
+
+
+@app.route('/predict', methods=['POST'])
+async def predict(request):
+    # get data from web form
+    img_data = await request.form()
+    # read image data from form
+    img_bytes = await (img_data['file'].read())
+    # convert bytes to Image object
+    img = open_image(BytesIO(img_bytes))
+    # Make prediction
+    prediction = learn.predict(img)[0]
+    return JSONResponse({'result': str(prediction)})
+
+
+if __name__ == '__main__':
+    if 'serve' in sys.argv:
+        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
--- static/js/Client.js
+++ static/js/Client.js
@@ -1,40 +1,47 @@
 var el = x => document.getElementById(x);
 
 function showPicker() {
-  el("file-input").click();
+    // Emulates input type='file' behavior
+    el("file-input").click();
 }
 
 function showPicked(input) {
-  el("upload-label").innerHTML = input.files[0].name;
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    el("image-picked").src = e.target.result;
-    el("image-picked").className = "";
-  };
-  reader.readAsDataURL(input.files[0]);
+    el("upload-label").innerHTML = input.files[0].name;
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        el("image-picked").src = e.target.result;
+        el("image-picked").className = "";
+    };
+    reader.readAsDataURL(input.files[0]);
 }
 
 function analyze() {
-  var uploadFiles = el("file-input").files;
-  if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
+    var uploadFiles = el("file-input").files;
+    if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
 
-  el("analyze-button").innerHTML = "Predicting...";
-  var xhr = new XMLHttpRequest();
-  var loc = window.location;
-  xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
-    true);
-  xhr.onerror = function() {
-    alert(xhr.responseText);
-  };
-  xhr.onload = function(e) {
-    if (this.readyState === 4) {
-      var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `Result = ${response["result"]}`;
-    }
-    el("analyze-button").innerHTML = "Predict";
-  };
+    el("analyze-button").innerHTML = "Predicting...";
+    var xhr = new XMLHttpRequest();
+    // Gets currently hosted address
+    var loc = window.location;
+    // Opens a post request
+    // forms link like eg. http://localhost:8080/predict
+    xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/predict`,
+            true);
+    // Alert on request error
+    xhr.onerror = function() {
+        alert(xhr.responseText);
+    };
+    // on success
+    xhr.onload = function(e) {
+        if (this.readyState === 4) {
+          var response = JSON.parse(e.target.responseText);
+          el("result-label").innerHTML = `Result = ${response["result"]}`;
+        }
+        el("analyze-button").innerHTML = "Predict";
+    };
 
-  var fileData = new FormData();
-  fileData.append("file", uploadFiles[0]);
-  xhr.send(fileData);
+    // send image file data to server
+    var fileData = new FormData();
+    fileData.append("file", uploadFiles[0]);
+    xhr.send(fileData);
 }
